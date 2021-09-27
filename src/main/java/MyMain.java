34855a456a6e7166602193520ee33c0d60b99afe
milanboga <23mboga@athenian.org>
2021-09-27 15:30:02 -0700

    I do not know what to do to get the correct values, please assist. I feel like the values I am getting are quite right, but of course there is the possibility that I am wrong.

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,8 +2,8 @@ public class MyMain {
     // Rolls a single die
     // Returns a random integer between 1 and 6
     public static int rollDie() {
-        // REPLACE YOUR CODE HERE
-        return -1;
+        int roll = (int) (Math.random() * 6) + 1;
+        return roll;
     }
 
     // Calculate the probability of rolling at least one 6 when rolling 
@@ -11,32 +11,72 @@ public class MyMain {
     // Returns in the answer as a double corresponding to the percentage
     // For example, 75.5% would be 75.5
     public static double probabilityOneSix() {
-        // REPLACE WITH YOUR CODE HERE
+        double count = 0.0;
 
         // Your code should roll 6 dice 10,000 times, so you should have a
         // for loop such as:
         for (int i = 0; i < 10000; i++) {
-
+            for(int j = 0; j < 6; j++) {
+                rollDie();
+                if(rollDie() == 6){
+                    count = count + 1;
+                    break;
+                }
+            }
         }
-
-        return -1.0;
+        count = (count / 100.0);
+        return count;
     }
 
     // Calculate the probability of rolling at least two 6's when rolling 
     // twelve dice. Uses 10000 trials.
     public static double probabilityTwoSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+
+        int sixcount = 0;
+        double count = 0.0;
+        for (int i = 0; i < 10000; i++) {
+            for(int j = 0; j < 12; j++) {
+                rollDie();
+                if(rollDie() == 6){
+                    sixcount += 1;
+                    if(sixcount == 2) {
+                        sixcount = 0;
+                        count += 1;
+                        break;
+                    }
+                }
+            }
+        }
+        count = (count / 100);
+        return count;
     }
 
     // Calculate the probability of rolling at least three 6's when rolling
     // eighteen dice. Uses 10000 trials.
     public static double probabilityThreeSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int sixcount = 0;
+        double count = 0.0;
+        for (int i = 0; i < 10000; i++) {
+            for(int j = 0; j < 18; j++) {
+                rollDie();
+                if(rollDie() == 6){
+                    sixcount += 1;
+                    if(sixcount == 3) {
+                        sixcount = 0;
+                        count += 1;
+                        break;
+                    }
+                }
+            }
+        }
+        count = (count / 100);
+        return count;
     }
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        System.out.println(rollDie());
+        System.out.println(probabilityOneSix());
+        System.out.println(probabilityTwoSixes());
+        System.out.println(probabilityThreeSixes());
     }
 }
